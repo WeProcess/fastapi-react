5c0fdcf868b169221488a0b9a32d4832aa3a7595
WeProcess <bumblebee@karmamgmt.com>
2024-03-15 17:43:41 +0530

    15-03-24_backend

--- backend/app/api.py
+++ backend/app/api.py
@@ -48,7 +48,7 @@ async def login(db: db_dependency, form_data: OAuth2PasswordRequestForm = Depend
 
 ################# dashboard #################
 @app.get("/dashboard", tags=["Dashboard"])
-async def getdashboard(db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def get_dashboard(db: db_dependency, current_user: User = Depends(get_current_active_user)):
     # print(dashboard.getdashboard(db))
     kao_user = dashboard.getKAO(db)
     
@@ -56,41 +56,41 @@ async def getdashboard(db: db_dependency, current_user: User = Depends(get_curre
 
 ################# userTeam #################
 @app.get("/getUserTeam", tags=["User Team"])
-async def getUserTeam(db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def get_User_Team(db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"user_team" : userTeam.getUserTeam(db), "current_user" : current_user}
 
 @app.post("/addUserTeam", tags=["User Team"])
-async def addUserTeam(ut: schema.UserTeamBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def add_User_Team(ut: schema.UserTeamBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"user_team" : userTeam.addUserTeam(ut, db), "current_user" : current_user}
 
 @app.put("/updateUserTeam", tags=["User Team"])
-async def updateUserTeam(id:int, ut: schema.UserTeamBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def update_User_Team(id:int, ut: schema.UserTeamBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"user_team" : userTeam.updateUserTeam(id, ut, db), "current_user" : current_user}
 
 @app.delete("/deleteUserTeam", tags=["User Team"])
-async def deleteUserTeam(id:int, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def delete_User_Team(id:int, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"user_team" : userTeam.deleteUserTeam(id, db), "current_user" : current_user}
 
 ################# userType #################
 @app.get("/getUserType", tags=["User Type"])
-async def getUserType(db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def get_User_Type(db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"user_type" : userType.getUserType(db), "current_user" : current_user}
 
 @app.post("/addUserType", tags=["User Type"])
-async def addUserType(ut: schema.UserTypeBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def add_User_Type(ut: schema.UserTypeBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"user_type" : userType.addUserType(ut, db), "current_user" : current_user}
 
 @app.put("/updateUserType", tags=["User Type"])
-async def updateUserType(id:int, ut: schema.UserTypeBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def update_User_Type(id:int, ut: schema.UserTypeBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return userType.updateUserType(id, ut, db)
 
 @app.delete("/deleteUserType", tags=["User Type"])
-async def deleteUserType(id:int, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def delete_User_Type(id:int, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"user_type" : userType.deleteUserType(id, db), "current_user" : current_user}
 
 ################# user #################
 @app.get("/getUser", tags=["User"])
-async def getUser(db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def get_User(db: db_dependency, current_user: User = Depends(get_current_active_user)):
     user_var = user.getUser(db)
     for auser_var in user_var:
         print(auser_var.valuefile())
@@ -98,40 +98,40 @@ async def getUser(db: db_dependency, current_user: User = Depends(get_current_ac
     return {"user" : user_var, "current_user" : current_user}
 
 @app.post("/addUser", tags=["User"])
-async def addUser(ur: schema.UserInDB, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def add_User(ur: schema.UserInDB, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"user" : user.addUser(ur, db), "current_user" : current_user}
 
 @app.put("/updateUser", tags=["User"])
-async def updateUser(id:int, ur: schema.UserInDB, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def update_User(id:int, ur: schema.UserInDB, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"user" : user.updateUser(id, ur, db), "current_user" : current_user}
 
 @app.delete("/deleteUser", tags=["User"])
-async def deleteUser(id:int, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def delete_User(id:int, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"user" : user.deleteUser(id, db), "current_user" : current_user}
 
 ################# matrix #################
 @app.get("/getMatrix", tags=["Matrix"])
-async def getmatrix(db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def get_matrix(db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"matrix" : matrix.getmatrix(db), "current_user" : current_user}
 
 @app.post("/addMatrix", tags=["Matrix"])
-async def addmatrix(ur: schema.MatrixBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def add_matrix(ur: schema.MatrixBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"matrix" : matrix.addmatrix(ur, db), "current_user" : current_user}
 
 @app.put("/updateMatrix", tags=["Matrix"])
-async def updatematrix(id:int, ur: schema.MatrixBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def update_matrix(id:int, ur: schema.MatrixBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"matrix" : matrix.updatematrix(id, ur, db), "current_user" : current_user}
 
 @app.delete("/deleteMatrix", tags=["Matrix"])
-async def deletematrix(id:int, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def delete_matrix(id:int, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"matrix" : matrix.deletematrix(id, db), "current_user" : current_user}
 
 
 ################# Email #################
 @app.get("/getEmail", tags=["Email"])
-async def getemail(db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def get_email(db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"email" : email.getemail(db), "current_user" : current_user}
 
 @app.post("/addEmail", tags=["Email"])
-async def addemail(email: schema.EmailBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
+async def add_email(email: schema.EmailBase, db: db_dependency, current_user: User = Depends(get_current_active_user)):
     return {"email" : email.addemail(email, db), "current_user" : current_user}
